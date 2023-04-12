7d034472b79a93e5c6880555ddd0d17dbe195a2f
Maria Hensley <mscelie34@zoho.com>
2023-04-12 15:09:59 -0500

    classes exercise1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Audi";
+            myCar.Model = "A8";
+            myCar.Year = 2023;
+            
+            var acura = new Car()
+            {
+                Make = "Acura",
+                Model = "MDX",
+                Year = 2023
+            };
+
+            var hyundai = new Car("Hyundai", "Genesis", 2023);
+
+            var carList = new List<Car>() {myCar, acura,hyundai};
+            Console.WriteLine(carList.Capacity);
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
         }
     }
 }
